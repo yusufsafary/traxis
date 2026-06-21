1496ef3c1777da5dc61d32fd95f820ee742b3185
arthouse <146998950+yusufsafary@users.noreply.github.com>
2026-06-21 10:27:08 +0700

    add: vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  base: "/traxis/",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+    dedupe: ["react", "react-dom"],
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
